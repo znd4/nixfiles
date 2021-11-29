bfd7a020807e1183cf03bf76e15f66c01f111882
Zane Dufour <zane@znd4.me>
2021-11-28 22:27:59 -0500

    Refactor install.py, install starship.py and signal and lazygit

--- .dotfiles/install.py
+++ .dotfiles/install.py
@@ -2,33 +2,59 @@ from pyinfra.operations import apt, brew, server
 
 # Install firacode nerd fonts
 
-server.shell(
-    name="install joplin",
-    commands=["wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash"],
-)
+def main():
+    configure_repos()
+    install_packages()
+    brew_installs()
+    script_installs()
 
-"""
-sudo add-apt-repository ppa:appimagelauncher-team/stable
-sudo apt update
-sudo apt install appimagelauncher
-"""
-apt.ppa(
-    src="ppa:appimagelauncher-team/stable",
-    name="Add appimagelauncher repo",
-    sudo=True,
-)
+def configure_repos():
+    """
+    Configure OS repos (apt, pacman, yum etc.)
+    """
+    apt.ppa(
+        src="ppa:appimagelauncher-team/stable",
+        name="Add appimagelauncher repo",
+        sudo=True,
+    )
 
-server.packages(
-    packages=["appimagelauncher"],
-    sudo=True,
-)
+    apt.key(
+        name="Add signal desktop key",
+        src="https://updates.signal.org/desktop/apt/keys.asc",
+        sudo=True
+    )
 
-# TODO - Install firacode
+def brew_installs():
+    brew.packages(
+        name="Install lazygit",
+        packages=["jesseduffield/lazygit/lazygit"],
+    )
+
+def script_installs():
+    server.shell(
+        name="Install starship",
+        commands=[
+            'sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes',
+        ],
+        sudo=True,
+    )
+    
+    server.shell(
+        name="install joplin",
+        commands=["wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash"],
+    )
+
+def install_packages():
+    server.packages(
+        packages=["appimagelauncher"],
+        sudo=True,
+    )
+
+# TODO - Install firacode nerd font
 # TODO - Install vscode
-# TODO - Install starship
 # TODO - Install direnv
 # TODO - Install 
 
-# apt.packages(
-#     name="Install vscode",
-# )
+main()
+
+
