f73b9be2b1446f4e04827c50439e96c81e3cc928
Zane Dufour <zane@znd4.me>
2022-11-13 10:41:57 -0500

    add nvimtree mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -5,4 +5,8 @@ vimp.nnoremap("<leader>xw", ":TroubleToggle workspace_diagnostics<cr>")
 vimp.nnoremap("<leader>xd", ":TroubleToggle document_diagnostics<cr>")
 vimp.nnoremap("<leader>xq", ":TroubleToggle quickfix<cr>")
 vimp.nnoremap("<leader>xl", ":TroubleToggle loclist<cr>")
+
+vimp.nnoremap("<leader>nf", ":NvimTreeFocus<cr>")
+vimp.nnoremap("<leader>nt", ":NvimTreeToggle<cr>")
+
 vimp.nnoremap("gR", ":TroubleToggle lsp_references<cr>")
