405df0f8e5eb7d61f754c580db533a11c8411298
Zane Dufour <zane@znd4.me>
2022-12-15 07:25:20 -0500

    add vim.filetype.add to add terraform filetype configuration to neovim

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -33,3 +33,29 @@ end
 if vim.fn.executable("rg") ~= 1 then
     print("please brew install ripgrep")
 end
+
+vim.g.do_filetype_lua = 1
+vim.filetype.add({
+    extension = {
+        hcl = "hcl",
+        tf = "terraform",
+        tfvars = "terraform",
+        tfstate = "json",
+        ["tfstate.backup"] = "json",
+    },
+    filename = {
+        [".terraformrc"] = "hcl",
+        ["terraform.rc"] = "hcl",
+    },
+    pattern = {
+        [".*"] = {
+            priority = -math.huge,
+            function(path, bufnr)
+                local content = vim.filetype.getlines(bufnr, 1)
+                if vim.filetype.matchregex(content, [[^#!.*\<node\>]]) then
+                    return "javascript"
+                end
+            end,
+        },
+    },
+})
