83da304efe6d0208267b0506a0c432a39ccc3b4c
Zane Dufour <zane@znd4.me>
2023-01-14 20:22:54 -0500

    add new treesitter directive colzero

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -20,6 +20,25 @@ vim.filetype.add({
         ["justfile"] = "justfile",
     },
 })
+local function dump(o)
+    if type(o) == "table" then
+        local s = "{ "
+        for k, v in pairs(o) do
+            if type(k) ~= "number" then
+                k = '"' .. k .. '"'
+            end
+            s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+        end
+        return s .. "} "
+    else
+        return tostring(o)
+    end
+end
+vim.treesitter.query.add_directive("colzero!", function(match, pattern, bufnr, predicate, metadata)
+    for i, m in pairs(metadata) do
+        m.range[2] = 0
+    end
+end)
 
 require("nvim-treesitter.install").prefer_git = true
 
