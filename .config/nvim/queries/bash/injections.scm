ab0612ec9162874edcd3f05655c769406ae5ce2f
Zane Dufour <zane@znd4.me>
2023-01-14 20:20:00 -0500

    add some injections for bash and yaml

--- .config/nvim/queries/bash/injections.scm
+++ .config/nvim/queries/bash/injections.scm
@@ -7,3 +7,19 @@
  ;; Don't include the quotes in the capture
  (#offset! @jq 0 1 0 -1)
 )
+;; Write your query here like `(node) @capture`,
+;; put the cursor under the capture to highlight the matches.
+; heredoc with docker-compose yaml
+(
+ (pipeline
+   (redirected_statement
+     body: (command) @_cat (#eq? @_cat "cat")
+     )
+   (command
+     name: (command_name) @_command (#eq? @_command "docker-compose")
+   )
+ )
+ (heredoc_body
+    ) @yaml
+ (#offset! @yaml 0 0 -1 0)
+)
