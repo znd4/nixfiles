b61b50ec5e030c623ba24c052754a891f0165d69
Zane Dufour <zane@znd4.me>
2023-02-17 10:14:48 -0500

    add magic card wishlist template

--- .config/nvim/lua/config/orgmode.lua
+++ .config/nvim/lua/config/orgmode.lua
@@ -3,4 +3,12 @@ local org_dir = os.getenv("HOME") .. "/Documents/org"
 require("orgmode").setup({
     org_agenda_files = { org_dir .. "/*" },
     org_default_notes_file = org_dir .. "/refile.org",
+    org_agenda_templates = {
+        m = {
+            description = "Magic card wishlist",
+            target = org_dir .. "/magic_wishlist.org",
+            template = "%^{Card name}",
+        },
+        t = { description = "Task", template = "* TODO %?\n  %u" },
+    },
 })
