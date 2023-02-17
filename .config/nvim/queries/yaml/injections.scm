4aeb332f8b86c0421490e4b954dbea5894044d5c
Zane Dufour <zane@znd4.me>
2023-02-17 10:12:51 -0500

    add github workflows treesitter injections

--- .config/nvim/queries/yaml/injections.scm
+++ .config/nvim/queries/yaml/injections.scm
@@ -12,6 +12,36 @@
     )
   )
 
+;; github workflows
+(block_mapping_pair
+    key: (flow_node) @_jobs
+    (#eq? @_jobs "jobs")
+    value: (block_node (block_mapping (block_mapping_pair value: (block_node (block_mapping (block_mapping_pair 
+        key: (flow_node) @_steps
+        (#eq? @_steps "steps")
+        value: (block_node (block_sequence (block_sequence_item (block_node (block_mapping (block_mapping_pair 
+            key: (flow_node) @_run
+            (#eq? @_run "run")
+            value: (flow_node) @bash
+        ))))))
+    ))))))
+)
+(block_mapping_pair
+    key: (flow_node) @_jobs
+    (#eq? @_jobs "jobs")
+    value: (block_node (block_mapping (block_mapping_pair value: (block_node (block_mapping (block_mapping_pair 
+        key: (flow_node) @_steps
+        (#eq? @_steps "steps")
+        value: (block_node (block_sequence (block_sequence_item (block_node (block_mapping (block_mapping_pair 
+            key: (flow_node) @_run
+            (#eq? @_run "run")
+            value: (block_node (block_scalar) @bash)
+            (#offset! @bash 1)
+            (#colzero! @bash 1)
+        ))))))
+    ))))))
+)
+
 (block_mapping_pair
   key: (flow_node) @_run
   (#eq? @_run "run")
