4de4975b6adcb2fbc9f596fe0914742f7da19bf4
Zane Dufour <zane@znd4.me>
2023-03-23 10:47:39 -0400

    Add neorg (haven't really used much yet)

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -193,6 +193,24 @@ require("lazy").setup({
         },
     },
     {
+        "nvim-neorg/neorg",
+        build = ":Neorg sync-parsers",
+        opts = {
+            load = {
+                ["core.defaults"] = {}, -- Loads default behaviour
+                ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+                ["core.norg.dirman"] = { -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = "~/notes",
+                        },
+                    },
+                },
+            },
+        },
+        dependencies = { { "nvim-lua/plenary.nvim" } },
+    },
+    {
         "akinsho/toggleterm.nvim",
         version = "2.*",
         config = function()
