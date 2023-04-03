f7544b5e05b2168c07a0d36e1618688ab6ec1dab
Zane Dufour <zane@znd4.me>
2023-04-03 14:19:45 -0400

    properly call nix-shell in wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 return {
     adjust_window_size_when_changing_font_size = false,
     color_scheme = "neon-night (Gogh)",
-    default_prog = { "nix-shell", "--run", "zsh" },
+    default_prog = { "bash", "-c", "nix-shell --run zsh" },
     -- set font size to 14
     font_size = 14,
     -- set default font to Fira Code
