cff138af9bed4be3f30a30eb8ba539b7a1786884
Zane Dufour <zane@znd4.me>
2023-04-03 15:10:46 -0400

    add terminal fonts to config.nix with packageOverrides

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,1 +1,12 @@
-{ allowUnfree = true; }
+{
+  allowUnfree = true;
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "my-packages";
+      paths = [
+        victor-mono
+        fira-code
+      ];
+    };
+  };
+}
