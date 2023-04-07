37f219c4c0790cb61e4c603c4893874e4fc5e57a
Zane Dufour <zane@znd4.me>
2023-04-07 11:28:46 -0400

    Refactor to have shared packages across python environments

--- shell.nix
+++ shell.nix
@@ -1,21 +1,37 @@
 { pkgs ? import <nixpkgs> { } }:
+
 let
-  # Python dependencies
-  pythonPackages = with pkgs.python3Packages; [
-    nbconvert
-    ipython
+  universalPackages = [
+    "hatchling"
   ];
 
-  globalPackages = with  pkgs; [
-    # Note: to use use stable, just replace `nightly` with `stable`
-    # latest.rustChannels.nightly.rust
-    # latest.rustChannels.stable.rust
+  # Define function to create Python environment with specified packages and universal packages
+  createPythonEnv = { packages, withPackagesFn }: (withPackagesFn (ps:
+    with ps; packages ++ map (p: ps.${p}) universalPackages));
 
+  #k Create Python 3.10 environment
+  python310Packages = createPythonEnv {
+    packages = [
+      "numpy"
+      "pandas"
+    ];
+    withPackagesFn = pkgs.python310.withPackages;
+  };
 
-    # use 1passsword for git credentials
-    git-credential-1password
+  # Create Python 3.11 environment
+  python311Packages = createPythonEnv {
+    packages = [
+      "nbconvert"
+      "ipython"
+    ];
+    withPackagesFn = pkgs.python311.withPackages;
+  };
 
-    # Add some extra dependencies from `pkgs`
+  # Combine the two environments into a larger `pythonPackages` list
+  pythonPackages = [ python310Packages python311Packages ];
+
+  # Other dependencies
+  globalPackages = with pkgs; [
     clippy
     direnv
     fd
@@ -24,7 +40,6 @@ let
     github-cli
     go
     gum
-    hatch
     lazygit
     joplin
     nodejs
@@ -38,10 +53,6 @@ let
   ];
 
   rustPackages = with pkgs; [
-    # cargo
-    # rust-analyzer
-    # rustc
-    # rustfmt
     clang
     llvmPackages.bintools
     rustup
@@ -49,44 +60,12 @@ let
 
 in
 pkgs.mkShell rec {
-  name = "rust-env";
-
-  # RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
-  RUSTC_VERSION = "stable";
-  # https://github.com/rust-lang/rust-bindgen#environment-variables
-  LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-  shellHook = ''
-    export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-    export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-  '';
-  # Add libvmi precompiled library to rustc search path
-  RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
-    pkgs.libvmi
-  ]);
-  # Add libvmi, glibc, clang, glib headers to bindgen search path
-  BINDGEN_EXTRA_CLANG_ARGS =
-    # Includes with normal include path
-    (builtins.map (a: ''-I"${a}/include"'') [
-      pkgs.libvmi
-      pkgs.glibc.dev
-    ])
-    # Includes with special directory paths
-    ++ [
-      ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-      ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-      ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-    ];
-  depsBuildBuild = with pkgs; [ python3Packages.nbconvert ];
+  name = "my-env";
 
   buildInputs = with pkgs;
     [
-      rustPackages
       globalPackages
-
+      rustPackages
       pythonPackages
     ];
-  # Add some extra dependencies to buildInputs from `pkgs.python3Packages`
-
-  # Set Environment Variables
-  RUST_BACKTRACE = 1;
 }
