9a548ff2c0c05dd66a7155717fb3f67f2b02091d
Zane Dufour <zane@znd4.me>
2023-06-09 10:04:19 -0400

    automatically reload

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,1 +1,9 @@
+c.InteractiveShellApp.extensions = [
+    'autoreload'
+]
+# set autoreload to 2
+c.InteractiveShellApp.exec_lines = [
+    '%autoreload 2',
+]
 c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.editor='nvim'
