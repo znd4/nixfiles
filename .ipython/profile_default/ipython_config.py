95118f2ee9cf0ba25602f201ce89e8905de355a1
Zane Dufour <zane@znd4.me>
2023-06-21 14:33:44 -0400

    add ipython "editor" C-e shortcut

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,9 +1,19 @@
-c.InteractiveShellApp.extensions = [
-    'autoreload'
-]
+from IPython.core.pylabtools import backends
+from IPython.core.shellapp import backend_keys
+
+c = get_config()
+c.InteractiveShellApp.extensions = ["autoreload"]
 # set autoreload to 2
 c.InteractiveShellApp.exec_lines = [
-    '%autoreload 2',
+    "%autoreload 2",
 ]
-c.TerminalInteractiveShell.editing_mode = 'vi'
-c.TerminalInteractiveShell.editor='nvim'
+c.TerminalInteractiveShell.editing_mode = "vi"
+c.TerminalInteractiveShell.editor = "nvim"
+
+
+# iterm2 = "asyncio"
+# breakpoint()
+# backends[iterm2] = "module://matplotlib_iterm2.backend_iterm2"
+# backend_keys.append(iterm2)
+#
+# c.TerminalIPythonApp.pylab = iterm2
