e07fc9ef9e3990c096a070d854d349b5b96532b4
Zane Dufour <zane@znd4.me>
2023-07-20 19:25:15 -0400

    add obsidian.nvim plugin

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -126,7 +126,7 @@ require("nvim-treesitter.configs").setup({
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = { "org", "vim" },
+        additional_vim_regex_highlighting = { "org", "vim", "markdown" },
     },
     incremental_selection = {
         enable = true,
