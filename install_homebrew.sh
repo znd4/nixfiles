41bc3ddf3b576e49f9d8d6a676c5451ef8ef32e9
Zane Dufour <zane@znd4.me>
2023-09-07 10:39:45 -0700

    brew install parallel and stow

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 set -xe
+update_or_install_homebrew() {
+export PATH="$HOME/homebrew/bin:$PATH"
+# if homebrew is already installed, update it
+command -v brew && brew update && brew upgrade && return 0
 mkdir -p ~/homebrew
-curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/homebrew
+curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/homebrew    
+}
+update_or_install_homebrew
