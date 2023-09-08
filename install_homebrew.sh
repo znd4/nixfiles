04f896a6be1d5edc025840cf5013f7885f17fe67
Zane Dufour <zane@znd4.me>
2023-09-08 07:23:38 -0700

    use rbenv to install correct ruby version

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -xe
 update_or_install_homebrew() {
     export PATH="$HOME/homebrew/bin:$PATH"
@@ -8,18 +8,24 @@ update_or_install_homebrew() {
     curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/homebrew    
 }
 ensure_ruby_linux() {
-    # if ruby is already installed, return
-    command -v ruby && return 0
-    # if apt-get is not installed, return
-    command -v apt-get || return 0
-    # use sudo if not root
-    if [ "$(id -u)" -ne 0 ]; then
-        sudo apt-get update
-        sudo apt-get install -y ruby
-    else
-        apt-get update
-        apt-get install -y ruby
-    fi
+    # if on macos, return
+    [ "$(uname)" = "Darwin" ] && return 0
+
+    # if rbenv is 
+    export PATH="$HOME/.rbenv/bin:$PATH"
+    command -v rbenv || git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+    eval "$(rbenv init -)"
+
+
+    # if ruby-build dir is not present, clone it
+    [ -d ~/.rbenv/plugins/ruby-build ] || \
+        git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+    # add ruby-build to path
+    export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+
+    # if ruby 2.6 is not installed, install it
+    rbenv versions | grep 2.6 || rbenv install 2.6.8
 }
 update_or_install_homebrew
 ensure_ruby_linux
