ad07f5740c04cf4688ed10b62780f5ecd84497ff
Zane Dufour <zane@znd4.me>
2023-09-27 20:07:03 -0400

    switch wezterm to zellij

--- xdg-config/.config/wezterm/wezterm.lua
+++ xdg-config/.config/wezterm/wezterm.lua
@@ -19,14 +19,16 @@ config.keys = {}
 -- check if windows
 wezterm.log_error("target_triple: " .. wezterm.target_triple)
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    config.default_prog = { "wsl.exe", "-d", "debian", "--shell-type", "login", "--", "tmux", "new", "-Asdotfiles" }
+    local tmux_prog = { "wsl.exe", "-d", "debian", "--shell-type", "login", "--", "tmux", "new", "-Asdotfiles" }
 else
-    config.default_prog = {
+    local tmux_prog = {
         os.getenv("SHELL"),
         "-c",
         "tmux new -Asdotfiles",
     }
 end
+
+config.default_prog = { os.getenv("SHELL"), "-c", "zellij" }
 
 config.launch_menu = {
     {
