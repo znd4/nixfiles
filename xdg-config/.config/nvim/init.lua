3654f400954e905c3d40223d5fb63c178a6d4f47
Zane Dufour <zane@znd4.me>
2023-10-13 10:24:52 -0700

    move init.vim to init.lua

--- xdg-config/.config/nvim/init.lua
+++ xdg-config/.config/nvim/init.lua
@@ -5,12 +5,16 @@ vim.g.mapleader = " "
 
 vim.g.python3_host_prog = "python3"
 
+-- set clipboard+=unnamedplus
+vim.g.clipboard:append({ "unnamedplus" })
+
+
 -- TODO - use `nvm exec 17 which node` to set this
 -- vim.g.copilot_node_command = "/Users/zdufour/.nvm/versions/node/v17.9.1/bin/node"
 
 if vim.g.neovide ~= nil then
-    print("changing directory to home")
-    vim.cmd.cd()
+  print("changing directory to home")
+  vim.cmd.cd()
 end
 
 require("colorscheme")
@@ -24,10 +28,10 @@ require("mappings")
 require("commands")
 
 if vim.fn.executable("fd") ~= 1 then
-    print("please brew install fd")
+  print("please brew install fd")
 end
 if vim.fn.executable("rg") ~= 1 then
-    print("please brew install ripgrep")
+  print("please brew install ripgrep")
 end
 
 require("filetype")
