znd4/nixfiles

commit 32da607e15551c74708e88602f3692bf134c7bec

Update nixos-module.nix (#747)
flipping serviceConfig.Restart to "yes"

nix/nixos-module.nix
@@ -126,7 +126,7 @@ let
       value = {
         description = "KMonad for ${keyboard.device}";
         script = lib.escapeShellArgs cmd;
-        serviceConfig.Restart = "no";
+        serviceConfig.Restart = "yes";
         serviceConfig.User = "kmonad";
         serviceConfig.SupplementaryGroups = groups;
         serviceConfig.Nice = -20;

commit 5a7c8c25113393430e0a5aa638a3ee6eb4a8db78

nix: Fix serviceConfig.Restart
Fixes: 32da607e15551c74708e88602f3692bf134c7bec
See also: https://github.com/kmonad/kmonad/commit/32da607e15551c74708e88602f3692bf134c7bec#commitcomment-130168001

nix/nixos-module.nix
@@ -126,7 +126,7 @@ let
       value = {
         description = "KMonad for ${keyboard.device}";
         script = lib.escapeShellArgs cmd;
-        serviceConfig.Restart = "yes";
+        serviceConfig.Restart = "always";
         serviceConfig.User = "kmonad";
         serviceConfig.SupplementaryGroups = groups;
         serviceConfig.Nice = -20;