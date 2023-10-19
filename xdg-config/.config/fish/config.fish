4d183141297502e1ad82d2f2484eed58d5aeafb3
Zane Dufour <znd4@pm.me>
2023-10-19 12:39:34 -0700

    add separate path_functions conf.d file

--- xdg-config/.config/fish/config.fish
+++ xdg-config/.config/fish/config.fish
@@ -34,13 +34,6 @@ function interactive_setup
 	set -gx PKG_CONFIG_PATH /usr/lib/x86_64-linux-gnu/pkgconfig
 end
 
-function add_to_path
-    # guard clause for empty arg
-    test -z $argv[1]; and echo "add_to_path: empty arg" >&2; and return 1
-    # guard clause for non-directory arg
-    test -d $argv[1]; or echo "add_to_path: not a directory" >&2; and return 2
-    set -gx PATH $argv[1] $PATH
-end
 
 function setup_zoxide
 	zoxide init fish | source
