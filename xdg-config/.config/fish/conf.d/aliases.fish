60bed6b845f9ed77ba9ca84ee478ec2c7f77e71d
Zane Dufour <znd4@pm.me>
2023-10-21 19:05:34 -0700

    bathelp fish alias

--- xdg-config/.config/fish/conf.d/aliases.fish
+++ xdg-config/.config/fish/conf.d/aliases.fish
@@ -9,6 +9,8 @@ alias grep='grep --color=auto'
 
 alias k="kubectl"
 
+alias bathelp="bat --language=help --plain"
+
 # colored GCC warnings and errors
 #export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
