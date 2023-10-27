e04fe1a667fcd20c55b0f502b2db2d49c8f966dd
Zane Dufour <znd4@pm.me>
2023-10-27 11:11:58 -0700

    move flash.nvim into separate file

--- xdg-config/.config/nvim/lua/config/lazy.lua
+++ xdg-config/.config/nvim/lua/config/lazy.lua
@@ -275,39 +275,6 @@ require("lazy").setup({
   },
 
   -- Colorschemes
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {},
-    keys = {
-      {
-        "s",
-        mode = { "n", "x", "o" },
-        function()
-          -- default options: exact mode, multi window, all directions, with a backdrop
-          require("flash").jump()
-        end,
-        desc = "Flash",
-      },
-      {
-        "S",
-        mode = { "n", "o", "x" },
-        function()
-          require("flash").treesitter()
-        end,
-        desc = "Flash Treesitter",
-      },
-      {
-        "r",
-        mode = "o",
-        function()
-          require("flash").remote()
-        end,
-        desc = "Remote Flash",
-      },
-    },
-  },
 
   {
     "phaazon/hop.nvim",
