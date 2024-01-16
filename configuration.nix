fd64b6be8ca4aec4f8e0aa95753a3442808c96a7
Zane Dufour <zane@znd4.me>
2024-01-16 10:46:40 -0500

    Revert "add wezterm"
    This reverts commit e3b79b54dc31b49302d0a12ec1812f38e481126a.

--- configuration.nix
+++ configuration.nix
@@ -147,7 +147,6 @@
     tmux
     unzip
     usbutils
-    wezterm
     wget
     zig
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
