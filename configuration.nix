e3b79b54dc31b49302d0a12ec1812f38e481126a
Zane Dufour <zane@znd4.me>
2024-01-16 10:45:21 -0500

    add wezterm

--- configuration.nix
+++ configuration.nix
@@ -147,6 +147,7 @@
     tmux
     unzip
     usbutils
+    wezterm
     wget
     zig
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
