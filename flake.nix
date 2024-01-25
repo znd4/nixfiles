63f9889ecebec3a4bab3dbdeb90d1a11280be1ca
Zane Dufour <zane@znd4.me>
2024-01-25 15:15:34 -0500

    change stateVersion to 23.05

--- flake.nix
+++ flake.nix
@@ -14,9 +14,6 @@
     , ...
     } @ inputs:
 
-    let
-      stateVersion = "23.11";
-    in
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -35,9 +32,9 @@
           # TODO - define closure that accepts username and system as arguments
         in
         {
-          znd4 = home-manager.lib.homeManagerConfiguration {
+          "znd4@nixos" = home-manager.lib.homeManagerConfiguration {
             pkgs = nixpkgs.legacyPackages.x86_64-linux;
-            extraSpecialArgs = { inherit inputs; outputs = self; username = "znd4"; stateVersion = stateVersion; };
+            extraSpecialArgs = { inherit inputs; outputs = self; username = "znd4"; stateVersion = "23.05"; };
             modules = [
               ./home-manager/home.nix
             ];
