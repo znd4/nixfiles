874722fed30994fb89c6921cf2fbb8ee77f91e96
Zane Dufour <zane@znd4.me>
2024-01-25 15:06:46 -0500

    make git config a single object instead of list

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -62,54 +62,54 @@
   programs.home-manager.enable = true;
   programs.git = {
     enable = true;
-    config = [
-      {
-        init.defaultBranch = "main";
-        user = {
-          name = "Zane Dufour";
-          email = "zane@znd4.dev";
-        };
-        push.autoSetupRemote = true;
-        pull.rebase = false;
-      }
-      {
-        alias = {
-          a = "add";
-          pl = "pull";
-          c = "commit";
-          cm = "commit";
-          co = "checkout";
-          ck = "checkout";
-          s = "status";
-          ps = "push";
-          d = "diff";
-        };
-      }
+    config = {
+
+      init.defaultBranch = "main";
+      user = {
+        name = "Zane Dufour";
+        email = "zane@znd4.dev";
+      };
+      push.autoSetupRemote = true;
+      pull.rebase = false;
+
+
+      alias = {
+        a = "add";
+        pl = "pull";
+        c = "commit";
+        cm = "commit";
+        co = "checkout";
+        ck = "checkout";
+        s = "status";
+        ps = "push";
+        d = "diff";
+      };
+
       # delta configuration
-      {
-        core = {
-          pager = "delta";
-        };
-        interactive = {
-          diffFilter = "delta --color-only";
-        };
-        delta = {
-          pager = "less";
-          navigate = true;
-          light = false;
-          side-by-side = true;
-        };
-        pager = {
-          diff = "delta";
-          log = "delta";
-          reflog = "delta";
-        };
-        diff = {
-          colorMoved = "default";
-          pager = "delta";
-        };
-      }
-    ];
+
+      core = {
+        pager = "delta";
+      };
+      interactive = {
+        diffFilter = "delta --color-only";
+      };
+      delta = {
+        pager = "less";
+        navigate = true;
+        light = false;
+        side-by-side = true;
+      };
+      pager = {
+        diff = "delta";
+        log = "delta";
+        reflog = "delta";
+      };
+      diff = {
+        colorMoved = "default";
+        pager = "delta";
+      };
+
+    };
 
   };
   programs.tmux = {
