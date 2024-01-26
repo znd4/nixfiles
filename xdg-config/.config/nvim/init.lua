0c4e55d07d69e988b3403809ba58390ea6a99480
Zane Dufour <zane@znd4.me>
2024-01-25 22:42:04 -0500

    make clipboard use cb

--- xdg-config/.config/nvim/init.lua
+++ xdg-config/.config/nvim/init.lua
@@ -7,7 +7,18 @@ vim.g.mapleader = " "
 vim.g.python3_host_prog = "python3"
 
 -- set clipboard+=unnamedplus
-vim.o.clipboard = vim.o.clipboard .. "unnamedplus"
+vim.g.clipboard = {
+  name = "custom_clipboard",
+  copy = {
+    ["+"] = { "cb", "copy" },
+    ["*"] = { "cb", "copy" },
+  },
+  paste = {
+    ["+"] = { "cb", "paste" },
+    ["*"] = { "cb", "paste" },
+  },
+  cache_enabled = 1,
+}
 
 -- TODO - use `nvm exec 17 which node` to set this
 -- vim.g.copilot_node_command = "/Users/zdufour/.nvm/versions/node/v17.9.1/bin/node"
