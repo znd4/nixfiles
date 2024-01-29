971581fa512bd912e7ab3914561ed22e6d5b924d
Zane Dufour <zane@znd4.me>
2024-01-29 15:36:43 -0500

    add httpie

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -73,7 +73,7 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [ httpie ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
