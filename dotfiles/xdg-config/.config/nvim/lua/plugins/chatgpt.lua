6f93252b4a7dda16e4ce8124eccca9eb4b498cf8
Zane Dufour <zane@znd4.me>
2024-02-07 16:58:37 -0500

    only start chatgpt when I run one of the commands

--- dotfiles/xdg-config/.config/nvim/lua/plugins/chatgpt.lua
+++ dotfiles/xdg-config/.config/nvim/lua/plugins/chatgpt.lua
@@ -4,6 +4,13 @@ return {
   config = {
     api_key_cmd = "op read op://private/openai/credential --no-newline",
   },
+  cmd = {
+    "ChatGPT",
+    "ChatGPTRun",
+    "ChatGPTActAs",
+    "ChatGPTCompleteCode",
+    "ChatGPTEditWithInstructions",
+  },
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
