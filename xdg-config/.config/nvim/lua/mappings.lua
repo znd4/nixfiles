b2a111ee895b2ffac3626d983f6fca5b4ba71c56
Zane Dufour <znd4@pm.me>
2024-02-08 07:34:49 -0800

    don't change escape mapping in vscode

--- xdg-config/.config/nvim/lua/mappings.lua
+++ xdg-config/.config/nvim/lua/mappings.lua
@@ -59,9 +59,6 @@ local function escape()
   -- check if a quickfix window is open
   vim.cmd("cclose")
 end
-if not vim.g.vscode then
-  vimp.nnoremap("<esc>", escape)
-end
 
 local leader = "<leader>"
 vimp.nnoremap(leader .. "fo", factory(vim.cmd.Octo, "actions"))
