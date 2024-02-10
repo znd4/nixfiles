507e9bb4f54caa176b43b6281f2ebdd4b7d97d1b
Zane Dufour <zane@znd4.me>
2024-02-10 11:51:00 -0500

    add kmonad in overlay

--- nixos/default.nix
+++ nixos/default.nix
@@ -145,21 +145,7 @@ in {
 
   # logseq
   nixpkgs.config.permittedInsecurePackages = [ "electron-20.3.12" ];
-  nixpkgs.overlays = [
-    (final: prev: {
-      logseq = prev.logseq.overrideAttrs (oldAttrs: {
-        postFixup = ''
-          makeWrapper ${prev.electron_20}/bin/electron $out/bin/${oldAttrs.pname} \
-            --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
-            --add-flags $out/share/${oldAttrs.pname}/resources/app \
-            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
-            --prefix LD_LIBRARY_PATH : "${
-              prev.lib.makeLibraryPath [ prev.stdenv.cc.cc.lib ]
-            }"
-        '';
-      });
-    })
-  ];
+  nixpkgs.overlays = import ./overlays;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
---
+++ nixos/overlays/logseq.nix
@@ -0,0 +1,14 @@
+{ ... }:
+(final: prev: {
+  logseq = prev.logseq.overrideAttrs (oldAttrs: {
+    postFixup = ''
+      makeWrapper ${prev.electron_20}/bin/electron $out/bin/${oldAttrs.pname} \
+      --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
+      --add-flags $out/share/${oldAttrs.pname}/resources/app \
+      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
+      --prefix LD_LIBRARY_PATH : "${
+        prev.lib.makeLibraryPath [ prev.stdenv.cc.cc.lib ]
+      }"
+    '';
+  });
+})
