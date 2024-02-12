f12054dc9403c6cefee6f3b63f83ed226c87a10e
Zane Dufour <znd4@pm.me>
2024-02-12 13:52:43 -0800

    add nix settings

--- darwin/default.nix
+++ darwin/default.nix
@@ -1,8 +1,9 @@
-{pkgs, username, ...}: {
+{ pkgs, username, ... }: {
   users.users.${username} = {
     name = "${username}";
     home = "/Users/${username}";
     isHidden = false;
   };
+  nix.settings.experimental-features = "nix-command flakes";
   services.nix-daemon.enable = true;
 }
