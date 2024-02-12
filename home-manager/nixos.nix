c95732d31074c36f124eccb4072dd565b8c1d645
Zane Dufour <znd4@pm.me>
2024-02-12 12:22:21 -0800

    remove unnecessary outputs

--- home-manager/nixos.nix
+++ home-manager/nixos.nix
@@ -1,5 +1,5 @@
 
-{ inputs, outputs, username, lib, config, pkgs, stateVersion, ... }: {
+{ inputs, username, lib, config, pkgs, stateVersion, ... }: {
   # You can import other home-manager modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
--- home-manager/shared.nix
+++ home-manager/shared.nix
@@ -1,6 +1,6 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-{ inputs, outputs, username, lib, config, pkgs, stateVersion, ... }: {
+{ inputs, username, lib, config, pkgs, stateVersion, ... }: {
 
   nixpkgs = {
     # You can add overlays here
