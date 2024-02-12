d06dd2b13bcfd9615cb2dd9281881d0b436677b9
Zane Dufour <znd4@pm.me>
2024-02-12 11:52:33 -0800

    don't enable direnv in shell.nix

--- shell/default.nix
+++ shell/default.nix
@@ -1,8 +1,5 @@
 { lib, config, pkgs, inputs, ... }: {
 
-  programs.direnv = {
-    enable = true;
-  };
   programs.fish.enable = lib.mkDefault true;
   programs.starship.enable = true;
   programs.skim.fuzzyCompletion = true;
