9b469ed95b6960cd9acef1fbcf0ce117ea31c394
Zane Dufour <znd4@pm.me>
2024-02-13 13:01:36 -0800

    add supervisor package

--- darwin/default.nix
+++ darwin/default.nix
@@ -3,37 +3,46 @@
     name = "${username}";
     home = "/Users/${username}";
     isHidden = false;
+    packages = with pkgs; [
+      python311Packages.supervisor
+    ];
   };
   nix.settings.experimental-features = "nix-command flakes";
   services.nix-daemon.enable = true;
   nixpkgs.overlays = [ inputs.kmonad.overlays.default ];
-  launchd.user.agents = {
-    kmonad = {
-      script = lib.strings.escapeShellArgs [
-        "kmonad"
-        "--log-level=debug"
-        (pkgs.writeTextFile {
-          name = "kmonad-config-with-header.kbd";
-          text = ''
-            ;; comment at beginning
-            (defcfg
-              input (iokit-name)
-              output (dext)
-              fallthrough true
-            )
-            ${builtins.readFile
-            "${inputs.dotfiles}/xdg-config/.config/kmonad/config.kbd"}
-          '';
-        })
-      ];
-      path = [ pkgs.kmonad ];
-      serviceConfig = {
-        Disable = true;
-        UserName = "root";
-        StandardOutPath = "/tmp/kmonad.log";
-        Debug = true;
-        KeepAlive = true;
-      };
-    };
-  };
+  # environment.postBuild 
+  # launchd.user.agents = {
+  #   kmonad = {
+  #     script = lib.strings.escapeShellArgs [
+  #       # "echo"
+  #       # "kmonad"
+  #       "/Users/${username}/.nix-profile/bin/kmonad"
+  #       "--log-level=debug"
+  #       (pkgs.writeTextFile {
+  #         name = "kmonad-config-with-header.kbd";
+  #         text = ''
+  #           ;; comment at beginning
+  #           (defcfg
+  #             input (iokit-name "Apple Internal Keyboard / Trackpad")
+  #             output (kext)
+  #             fallthrough true
+  #           )
+  #           ${builtins.readFile
+  #           "${inputs.dotfiles}/xdg-config/.config/kmonad/config.kbd"}
+  #         '';
+  #       })
+  #     ];
+  #     environment = {
+  #       PATH="/Users/${username}/.nix-profile/bin";
+  #     };
+  #     # path = [ pkgs.kmonad ];
+  #     serviceConfig = {
+  #       UserName = "root";
+  #       StandardOutPath = "/tmp/kmonad.out";
+  #       StandardErrorPath = "/tmp/kmonad.err";
+  #       Debug = true;
+  #       KeepAlive = true;
+  #     };
+  #   };
+  # };
 }
