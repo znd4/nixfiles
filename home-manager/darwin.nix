2d0e66b548d7757698322d9abc6ce48e9b6c5f14
Zane Dufour <znd4@pm.me>
2024-02-13 13:04:45 -0800

    add supervisor to homeManager

--- home-manager/darwin.nix
+++ home-manager/darwin.nix
@@ -1,6 +1,7 @@
 
 
-{inputs, username, stateVersion}: { lib, config, pkgs, ... }: {
+{ inputs, username, stateVersion }:
+{ lib, config, pkgs, ... }: {
   # You can import other home-manager modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
@@ -10,12 +11,14 @@
     # inputs.nix-colors.homeManagerModules.default
 
     # You can also split up your configuration and import pieces of it here:
-    (args: (import ./shared.nix) (args // {
-      inherit inputs;
-      inherit username;
-      inherit stateVersion;
-      inherit pkgs;
-    }))
+    (args:
+      (import ./shared.nix) (args // {
+        inherit inputs;
+        inherit username;
+        inherit stateVersion;
+        inherit pkgs;
+      }))
   ];
+  home.packages = with pkgs; [ python311Packages.supervisor ];
   home.stateVersion = stateVersion;
 }
