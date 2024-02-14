19ed09f990a7422fad77081cc128ff6bbd298cbf
Zane Dufour <znd4@pm.me>
2024-02-14 08:52:28 -0800

    make fish the default login shell in darwin

--- darwin/default.nix
+++ darwin/default.nix
@@ -7,5 +7,11 @@
   nix.settings.experimental-features = "nix-command flakes";
   services.nix-daemon.enable = true;
   nixpkgs.overlays = [ inputs.kmonad.overlays.default ];
+  environment.systemPackages = with pkgs; [
+    fish
+    zsh
+    git
+  ];
+  environment.loginShell = "fish --login";
   # environment.postBuild 
 }
