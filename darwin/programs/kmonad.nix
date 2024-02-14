84b938b8ca29923fce198af818a59f2f0b848029
Zane Dufour <znd4@pm.me>
2024-02-14 08:44:50 -0800

    make darwin kmonad use shell alias

--- darwin/programs/kmonad.nix
+++ darwin/programs/kmonad.nix
@@ -1,5 +1,23 @@
-{ lib, pkgs, inputs, username, ... }: {
+{ lib, pkgs, inputs, username, ... }:
+let
+  kmonadConfig = (pkgs.writeTextFile {
+    name = "kmonad-config-with-header.kbd";
+    text = ''
+      ;; comment at beginning
+      (defcfg
+       input (iokit-name "Apple Internal Keyboard / Trackpad")
+       output (kext)
+       fallthrough true
+      )
+      ${builtins.readFile
+      "${inputs.dotfiles}/xdg-config/.config/kmonad/config.kbd"}
+    '';
+  });
+in {
   nixpkgs.overlays = [ inputs.kmonad.overlays.default ];
+  environment.shellAliases = {
+    km = lib.escapeShellArgs [ "kmonad" kmonadConfig ];
+  };
   environment.systemPackages = with pkgs; [ kmonad ];
   # environment.postBuild = [
   #   lib.strings.escapeShellArgs
