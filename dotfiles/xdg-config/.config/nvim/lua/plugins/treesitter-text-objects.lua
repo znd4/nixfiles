0af415b0e87413b407e86901a1973c5d17a33833
Zane Dufour <zane@znd4.me>
2024-02-18 11:05:33 -0500

    move treesitter textobjects to standalone config

---
+++ dotfiles/xdg-config/.config/nvim/lua/plugins/treesitter-text-objects.lua
@@ -0,0 +1,68 @@
+return {
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  dependencies = ["nvim-treesitter/nvim-treesitter"],
+  opts = {
+    textobjects = {
+      select = {
+        enable = true,
+
+        -- Automatically jump forward to textobj, similar to targets.vim
+        lookahead = true,
+
+        keymaps = {
+          ["ab"] = "@block.outer",
+          ["ib"] = "@block.inner",
+
+          -- You can use the capture groups defined in textobjects.scm
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+
+          ["al"] = "@call.outer",
+          ["il"] = { query = "@call.inner", desc = "Select inner part of a function call" },
+        },
+        -- You can choose the select mode (default is charwise 'v')
+        selection_modes = {
+          ["@parameter.outer"] = "v", -- charwise
+          ["@function.outer"] = "V", -- linewise
+          ["@class.outer"] = "<c-v>", -- blockwise
+        },
+        -- If you set this to `true` (default is `false`) then any textobject is
+        -- extended to include preceding xor succeeding whitespace. Succeeding
+        -- whitespace has priority in order to act similarly to eg the built-in
+        -- `ap`.
+        -- include_surrounding_whitespace = true,
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          ["]m"] = "@function.outer",
+          ["]]"] = "@class.outer",
+        },
+        goto_next_end = {
+          ["]M"] = "@function.outer",
+          ["]["] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[m"] = "@function.outer",
+          ["[["] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[M"] = "@function.outer",
+          ["[]"] = "@class.outer",
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+          ["<leader>o"] = "@binary_operator",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+          ["<leader>O"] = "@binary_operator",
+        },
+      },
+    },
+  },
+}
