36b557329c07f8c44116d515b1b30852479049ce
Zane Dufour <znd4@pm.me>
2024-02-19 13:01:02 -0800

    fix treesitter textobjects dependency spec

--- dotfiles/xdg-config/.config/nvim/lua/plugins/treesitter-text-objects.lua
+++ dotfiles/xdg-config/.config/nvim/lua/plugins/treesitter-text-objects.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
-  dependencies = ["nvim-treesitter/nvim-treesitter"],
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
   opts = {
     textobjects = {
       select = {
