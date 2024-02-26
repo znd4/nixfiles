63b360e80449f19bb059ef74126fb5e80fd4f8c2
Zane Dufour <znd4@pm.me>
2024-02-26 10:03:35 -0800

    don't set SSH_AUTH_SOCK on darwin

--- home-manager/darwin.nix
+++ home-manager/darwin.nix
@@ -21,8 +21,8 @@
     ./shared.nix
     ./programs/kmonad.nix
   ];
-  home.sessionVariables.SSH_AUTH_SOCK =
-    "/Users/${username}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+  # home.sessionVariables.SSH_AUTH_SOCK =
+  #   "/Users/${username}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
   home.sessionPath = [ "/Users/${username}/homebrew/bin" ];
   home.packages = with pkgs; [ python311Packages.supervisor ];
   home.stateVersion = stateVersion;
