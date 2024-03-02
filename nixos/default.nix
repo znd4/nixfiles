1d012313642496d2e67e12f5a185f279ac3c4838
Zane Dufour <zane@znd4.dev>
2024-03-02 10:45:51 -0500

    add FiraCode

--- nixos/default.nix
+++ nixos/default.nix
@@ -35,7 +35,7 @@ in
   boot.loader.efi.canTouchEfiVariables = true;
 
   # font
-  fonts.packages = with pkgs; [ (nerdfonts.overrides { fonts = [ "VictorMono" ]; }) ];
+  fonts.packages = with pkgs; [ (nerdfonts.overrides { fonts = ["FiraCode" "VictorMono" ]; }) ];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
