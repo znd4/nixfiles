8dc67fde91f4248da8ebb496b247cd1a5c8e7b1d
Zane Dufour <znd4@pm.me>
2024-03-04 11:06:46 -0800

    more changes!!!

--- home-manager/darwin/default.nix
+++ home-manager/darwin/default.nix
@@ -2,10 +2,10 @@
   inputs,
   username,
   config,
-  stateVersion,
   keys,
   pkgs,
-  ...}:
+  ...
+}:
 {
   imports = (
     builtins.map (f: (import (./. + "/${f}"))) (
@@ -14,38 +14,28 @@
 
   );
 
-# You can import other home-manager modules here
-_module.args = {
-  inherit inputs;
-  inherit keys;
-  inherit username;
-# inherit pkgs;
-};
-home.sessionVariables.SSH_AUTH_SOCK = "/Users/${username}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
-home.homeDirectory = "/Users/${username}";
-home.sessionPath = [ "${config.home.homeDirectory}/homebrew/bin" ];
-home.packages = with pkgs; [ python311Packages.supervisor ];
-home.stateVersion = stateVersion;
-# programs.git.includes = [{
-#   condition = "gitdir:${config.home.homeDirectory}/Work";
-#   contents = {
-#     user = {
-#       name = "Zane Dufour";
-#       email = "extern.zane.dufour@vw.com";
-#       signingKey = keys."git.company.com";
-#     };
-#   };
-# }];
-# programs.ssh.matchBlocks = let
-#   vw_config = {
-#     identitiesOnly = true;
-#     identityFile =
-#       "${pkgs.writeText "vw_id_rsa.pub" keys."git.company.com"}";
-#   };
-# in {
-#   "git2.company.com" = vw_config;
-#   "git.company.com" = vw_config;
-# };
-
+  home.sessionVariables.SSH_AUTH_SOCK = "/Users/${username}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+  home.homeDirectory = "/Users/${username}";
+  home.sessionPath = [ "${config.home.homeDirectory}/homebrew/bin" ];
+  home.packages = with pkgs; [ python311Packages.supervisor ];
+  # programs.git.includes = [{
+  #   condition = "gitdir:${config.home.homeDirectory}/Work";
+  #   contents = {
+  #     user = {
+  #       name = "Zane Dufour";
+  #       email = "extern.zane.dufour@vw.com";
+  #       signingKey = keys."git.company.com";
+  #     };
+  #   };
+  # }];
+  # programs.ssh.matchBlocks = let
+  #   vw_config = {
+  #     identitiesOnly = true;
+  #     identityFile =
+  #       "${pkgs.writeText "vw_id_rsa.pub" keys."git.company.com"}";
+  #   };
+  # in {
+  #   "git2.company.com" = vw_config;
+  #   "git.company.com" = vw_config;
+  # };
 }
-
