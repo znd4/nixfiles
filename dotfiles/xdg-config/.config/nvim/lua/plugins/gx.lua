8c4f501880c5259e1ac67c5a4a1509cc245e6ef9
Zane Dufour <zane@znd4.dev>
2024-03-06 10:02:52 -0500

    fix gx.nvim config

--- dotfiles/xdg-config/.config/nvim/lua/plugins/gx.lua
+++ dotfiles/xdg-config/.config/nvim/lua/plugins/gx.lua
@@ -6,7 +6,14 @@ end
 
 return {
   "chrishrb/gx.nvim",
-  event = { "BufEnter" },
+  keys = {
+    { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } },
+  },
+  cmd = { "Browse" },
+  init = function()
+    vim.g.netrw_nogx = 1
+  end,
   dependencies = { "nvim-lua/plenary.nvim" },
   config = true, -- default settings
+  submodules = false,
 }
