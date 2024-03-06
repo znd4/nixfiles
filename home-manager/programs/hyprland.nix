755c1e8072c80ed3d083eda47146c050302da589
Zane Dufour <zane@znd4.dev>
2024-03-06 10:58:16 -0500

    disable some animations

--- home-manager/programs/hyprland.nix
+++ home-manager/programs/hyprland.nix
@@ -197,16 +197,16 @@ else
 
           # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
 
-          bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-
-          animation = [
-            "windows, 1, 7, myBezier"
-            "windowsOut, 1, 7, default, popin 80%"
-            "border, 1, 10, default"
-            "borderangle, 1, 8, default"
-            "fade, 1, 7, default"
-            "workspaces, 1, 6, default"
-          ];
+          # bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+          #
+          # animation = [
+          #   "windows, 1, 7, myBezier"
+          #   "windowsOut, 1, 7, default, popin 80%"
+          #   "border, 1, 10, default"
+          #   "borderangle, 1, 8, default"
+          #   "fade, 1, 7, default"
+          #   "workspaces, 1, 6, default"
+          # ];
         };
 
         # Example windowrule v1
