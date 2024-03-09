c9b6d9c3b9f3c8c7609671b176ed5fab10350d43
Zane Dufour <zane@znd4.dev>
2024-03-09 10:02:40 -0500

    remove path_functions

--- dotfiles/fish/.config/fish/conf.d/001_path_functions.fish
+++
@@ -1,7 +0,0 @@
-function add_to_path
-    # guard clause for empty arg
-    test -z $argv[1]; and echo "add_to_path: empty arg" >&2; and return 1
-    # guard clause for non-directory arg
-    test -d $argv[1]; or echo "add_to_path: not a directory" >&2; and return 2
-    set -gx PATH $argv[1] $PATH
-end
