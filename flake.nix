94fda86ff7cead65017c8218ea0178e2b3d6a3aa
Zane Dufour <zane@znd4.dev>
2024-03-11 20:57:53 -0400

    move hyprland cachix location

--- flake.nix
+++ flake.nix
@@ -1,4 +1,8 @@
 {
+  nixConfig = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nil.url = "github:oxalica/nil";
