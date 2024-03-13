c3cb5b4ad409258442b74fbf99220bb4d67d109e
Zane Dufour <zane@znd4.dev>
2024-03-13 10:51:41 -0400

    remove _authenticate_hydroxide and run hydroxide serve

--- home-manager/programs/hydroxide.nix
+++ home-manager/programs/hydroxide.nix
@@ -1,9 +1,21 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [ hydroxide ];
-  # systemd.user.services.hydroxide = {
-  #   Unit = {
-  #     Description = "Open source protonmail bridge server";
-  #   };
-  # };
+  systemd.user.services.hydroxide = {
+    Unit = {
+      Description = "Open source protonmail bridge server";
+      Wants = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.hydroxide}/bin/hydroxide serve";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
 }
