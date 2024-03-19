c77ddf3582d5368d2d3217fed19b8d0024c30abd
Zane Dufour <zane@znd4.dev>
2024-03-19 13:45:22 -0400

    wrap in flake.parts

--- flake.nix
+++ flake.nix
@@ -1,11 +1,12 @@
 {
   nixConfig = {
-    extra-substituters = [ "https://hyprland.cachix.org" ];
+    extra-substituters = ["https://hyprland.cachix.org"];
     extra-trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nil.url = "github:oxalica/nil";
     nixd = {
@@ -64,58 +65,67 @@
   # };
   # inputs.kmonad.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      darwin,
-      ...
-    }@inputs:
-    let
-      lib = nixpkgs.lib;
-    in
-    {
-      keys = {
-        "github.com" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHkoZGPqvCciloARGk9/rgPdjCFI2JmsYbgboEv98RKc github.com key";
+  outputs = {
+    flake-parts,
+    nixpkgs,
+    darwin,
+    home-manager,
+    self,
+    ...
+  } @ inputs: let
+    lib = nixpkgs.lib;
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
+        formatter = pkgs.alejandra;
       };
-      darwinModules = {
-        default = ./darwin;
-      };
-      darwinFactory =
-        {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      flake = {
+        keys = {
+          "github.com" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHkoZGPqvCciloARGk9/rgPdjCFI2JmsYbgboEv98RKc github.com key";
+        };
+        darwinModules = {
+          default = ./darwin;
+        };
+        darwinFactory = {
           system ? "aarch64-darwin",
-          extraModules ? [ ],
+          extraModules ? [],
           username,
           stateVersion,
         }:
-        darwin.lib.darwinSystem {
-          system = system;
-          inherit inputs;
-          specialArgs = {
-            inherit inputs;
-            username = username;
-            stateVersion = stateVersion;
+          darwin.lib.darwinSystem {
             system = system;
+            inherit inputs;
+            specialArgs = {
+              inherit inputs;
+              username = username;
+              stateVersion = stateVersion;
+              system = system;
+            };
+            modules = [self.darwinModules.default] ++ extraModules;
           };
-          modules = [ self.darwinModules.default ] ++ extraModules;
+        darwinConfigurations.work = self.darwinFactory {
+          username = "dufourz";
+          stateVersion = 4;
         };
-      darwinConfigurations.work = self.darwinFactory {
-        username = "dufourz";
-        stateVersion = 4;
-      };
 
-      nixosConfigurations = (
-        builtins.listToAttrs (
-          builtins.map
+        nixosConfigurations = (
+          builtins.listToAttrs (
+            builtins.map
             (
               {
                 system ? "x86_64-linux",
                 stateVersion ? "23.11",
                 username,
                 hostname,
-              }:
-              (lib.attrsets.nameValuePair hostname (
+              }: (lib.attrsets.nameValuePair hostname (
                 lib.nixosSystem {
                   system = system;
                   specialArgs = {
@@ -143,48 +153,44 @@
                 username = "znd4";
               }
             ]
-        )
-      );
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
-      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt-rfc-style;
+          )
+        );
 
-      homeModules = {
-        default = ./home-manager;
-      };
-      homeConfigurationFactory =
-        {
+        homeModules = {
+          default = ./home-manager;
+        };
+        homeConfigurationFactory = {
           system,
           username,
           hostname,
           keys ? self.keys,
           stateVersion ? "23.11",
-          extraModules ? [ ],
+          extraModules ? [],
         }:
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.${system};
-          extraSpecialArgs = {
-            inherit
-              inputs
-              system
-              username
-              hostname
-              stateVersion
-              keys
-              ;
+          home-manager.lib.homeManagerConfiguration {
+            pkgs = nixpkgs.legacyPackages.${system};
+            extraSpecialArgs = {
+              inherit
+                inputs
+                system
+                username
+                hostname
+                stateVersion
+                keys
+                ;
+            };
+            modules = [self.homeModules.default] ++ extraModules;
           };
-          modules = [ self.homeModules.default ] ++ extraModules;
-        };
-      homeConfigurations = (
-        builtins.listToAttrs (
-          builtins.map
+        homeConfigurations = (
+          builtins.listToAttrs (
+            builtins.map
             (
               {
                 username,
                 hostname,
                 system ? "x86_64-linux",
-              }:
-              (lib.attrsets.nameValuePair "${username}@${hostname}" (
-                self.homeConfigurationFactory { inherit system username hostname; }
+              }: (lib.attrsets.nameValuePair "${username}@${hostname}" (
+                self.homeConfigurationFactory {inherit system username hostname;}
               ))
             )
             [
@@ -197,7 +203,8 @@
                 hostname = "t470";
               }
             ]
-        )
-      );
+          )
+        );
+      };
     };
 }
