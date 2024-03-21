4b99cac906d83ec11b57cbe2290a4e3eeb58fd9c
Zane Dufour <zane@znd4.dev>
2024-03-21 14:19:01 -0400

    try debugging dap load

--- dotfiles/xdg-config/.config/nvim/lua/plugins/dap.lua
+++ dotfiles/xdg-config/.config/nvim/lua/plugins/dap.lua
@@ -1,44 +1,53 @@
-local function dap_python_setup()
-  require("dap-python").setup(vim.g.python3_host_prog)
-  require("dap-python").test_runner = "pytest"
-end
-local function dap_go_setup()
-  local dapgo = require("dap-go")
-  dapgo.setup()
-end
+local function dap_go_setup() end
 return {
   "mfussenegger/nvim-dap",
   dependencies = { "leoluz/nvim-dap-go", "mfussenegger/nvim-dap-python", "nvim-lua/plenary.nvim" },
-  config = function()
-    dap_python_setup()
-    dap_go_setup()
-    -- local async = require("plenary.async").async
-    -- local dap_go = async(dap_go_setup)()
-    -- local dap_python = async(dap_python_setup)()
-    -- dap_go:await()
-    -- dap_python:await()
+  init = function()
+    print("loading dap")
+    print("python3_host_prog: " .. vim.g.python3_host_prog)
 
-    local vimp = require("vimp")
+    require("dap-python").setup(vim.g.python3_host_prog or "python3", {})
+    require("dap-python").test_runner = "pytest"
+
+    require("dap-go").setup()
+
+    local wk = require("which-key")
     local dap = require("dap")
-    vimp.nnoremap({ "silent" }, "<F5>", dap.continue, { desc = "debugger continue" })
-    vimp.nnoremap({ "silent" }, "<F10>", dap.step_over, { desc = "debugger step over" })
-    vimp.nnoremap({ "silent" }, "<F11>", dap.step_into, { desc = "debugger step into" })
-    vimp.nnoremap({ "silent" }, "<F12>", dap.step_out, { desc = "debugger step out" })
-    vimp.nnoremap({ "silent" }, "<Leader>b", dap.toggle_breakpoint, { desc = "debugger toggle breakpoint" })
-    vimp.nnoremap({ "silent" }, "<Leader>B", function()
-      require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-    end, { desc = "debugger set breakpoint condition" })
-    vimp.nnoremap({ "silent" }, "<Leader>lp", function()
-      require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-    end, { desc = "debugger set log point message" })
-    vimp.nnoremap({ "silent" }, "<Leader>ds", function()
-      local widgets = require("dap.ui.widgets")
-      widgets.centered_float(widgets.scopes)
-    end, { desc = "Open scopes in sidebar" })
-    vimp.nnoremap({ "silent" }, "<Leader>dh", function()
-      require("dap.ui.widgets").hover()
-    end, { desc = "View value of expression under cursor" })
-    vimp.nnoremap({ "silent" }, "<Leader>dr", dap.repl.open, { desc = "open debugger repl" })
-    vimp.nnoremap({ "silent" }, "<Leader>dl", dap.run_last, { desc = "run last debugger" })
+    wk.register({
+      ["<F5>"] = { dap.continue, "Continue" },
+      ["<F10>"] = { dap.step_over, "Step over" },
+      ["<F11>"] = { dap.step_into, "Step into" },
+      ["<F12>"] = { dap.step_out, "Step out" },
+    })
+    wk.register({
+      ["b"] = { dap.toggle_breakpoint, "Toggle breakpoint" },
+      ["B"] = {
+        function()
+          require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+        end,
+        "Set breakpoint condition",
+      },
+      ["lp"] = {
+        function()
+          require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+        end,
+        "Set log point message",
+      },
+      ["ds"] = {
+        function()
+          local widgets = require("dap.ui.widgets")
+          widgets.centered_float(widgets.scopes)
+        end,
+        "Open scopes in sidebar",
+      },
+      ["dh"] = {
+        function()
+          require("dap.ui.widgets").hover()
+        end,
+        "View value of expression under cursor",
+      },
+      ["dr"] = { dap.repl.open, "Open debugger repl" },
+      ["dl"] = { dap.run_last, "Run last debugger" },
+    }, { prefix = "<Leader>" })
   end,
 }
