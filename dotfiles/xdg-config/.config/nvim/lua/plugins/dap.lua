986273abe86d0aeb95e5c4deb1b4cc7c2c47acc6
Zane Dufour <zane@znd4.dev>
2024-03-21 14:28:28 -0400

    change dap mappings to not require F*

--- dotfiles/xdg-config/.config/nvim/lua/plugins/dap.lua
+++ dotfiles/xdg-config/.config/nvim/lua/plugins/dap.lua
@@ -1,11 +1,7 @@
-local function dap_go_setup() end
 return {
   "mfussenegger/nvim-dap",
   dependencies = { "leoluz/nvim-dap-go", "mfussenegger/nvim-dap-python", "nvim-lua/plenary.nvim" },
   init = function()
-    print("loading dap")
-    print("python3_host_prog: " .. vim.g.python3_host_prog)
-
     require("dap-python").setup(vim.g.python3_host_prog or "python3", {})
     require("dap-python").test_runner = "pytest"
 
@@ -20,34 +16,43 @@ return {
       ["<F12>"] = { dap.step_out, "Step out" },
     })
     wk.register({
-      ["b"] = { dap.toggle_breakpoint, "Toggle breakpoint" },
-      ["B"] = {
-        function()
-          require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-        end,
-        "Set breakpoint condition",
-      },
       ["lp"] = {
         function()
           require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
         end,
         "Set log point message",
       },
-      ["ds"] = {
-        function()
-          local widgets = require("dap.ui.widgets")
-          widgets.centered_float(widgets.scopes)
-        end,
-        "Open scopes in sidebar",
+      d = {
+        b = { dap.toggle_breakpoint, "Toggle breakpoint" },
+        B = {
+          function()
+            require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+          end,
+          "Set breakpoint condition",
+        },
+        w = {
+          function()
+            local widgets = require("dap.ui.widgets")
+            widgets.centered_float(widgets.scopes)
+          end,
+          "Open scopes in sidebar",
+        },
+        h = {
+          function()
+            require("dap.ui.widgets").hover()
+          end,
+          "View value of expression under cursor",
+        },
+
+        c = { dap.continue, "Open variables in sidebar" },
+        n = { dap.step_over, "Step over" },
+        s = { dap.step_into, "Step into" },
+        u = { dap.up, "Step out" },
+        d = { dap.down, "Step down" },
+
+        r = { dap.repl.open, "Open debugger repl" },
+        l = { dap.run_last, "Run last debugger" },
       },
-      ["dh"] = {
-        function()
-          require("dap.ui.widgets").hover()
-        end,
-        "View value of expression under cursor",
-      },
-      ["dr"] = { dap.repl.open, "Open debugger repl" },
-      ["dl"] = { dap.run_last, "Run last debugger" },
     }, { prefix = "<Leader>" })
   end,
 }
