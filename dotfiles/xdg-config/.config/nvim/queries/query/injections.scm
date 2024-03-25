38510527e871e625f1fd822f9db65d904158d55b
Zane Dufour <extern.zane.dufour@vw.com>
2024-03-25 10:06:11 -0700

    remove query inection for now

--- dotfiles/xdg-config/.config/nvim/queries/query/injections.scm
+++ dotfiles/xdg-config/.config/nvim/queries/query/injections.scm
@@ -1,12 +1,12 @@
-(
- predicate
- name: (identifier) @name
- (#eq? @name "match")
- parameters: (parameters
-  (string) @injection.content
-  (#set! injection.language "regex")
-  (#set! injection.include-children true)
-  ; trim first and last characters (quote characters)
-  (#offset! injection.content 1 0 -1 0)
- )
-)
+; (
+;  predicate
+;  name: (identifier) @name
+;  (#eq? @name "match")
+;  parameters: (parameters
+;   (string) @injection.content
+;   (#set! injection.language "regex")
+;   (#set! injection.include-children true)
+;   ; trim first and last characters (quote characters)
+;   (#offset! injection.content 1 0 -1 0)
+;  )
+; )
