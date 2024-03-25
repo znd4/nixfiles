2dea4721826a41abdf613bfd045989a83ca05d98
Zane Dufour <extern.zane.dufour@vw.com>
2024-03-25 10:34:55 -0700

    re-add query injection

--- dotfiles/xdg-config/.config/nvim/queries/query/injections.scm
+++ dotfiles/xdg-config/.config/nvim/queries/query/injections.scm
@@ -1,12 +1,12 @@
-; (
-;  predicate
-;  name: (identifier) @name
-;  (#eq? @name "match")
-;  parameters: (parameters
-;   (string) @injection.content
-;   (#set! injection.language "regex")
-;   (#set! injection.include-children true)
-;   ; trim first and last characters (quote characters)
-;   (#offset! injection.content 1 0 -1 0)
-;  )
-; )
+;; Write your query here like `(node) @capture`,
+;; put the cursor under the capture to highlight the matches.
+(predicate
+  name: (identifier) @match
+  (#eq? @match "match")
+  parameters: (parameters
+    (string) @pattern
+    (#offset! @pattern 1 0 -1 0)
+    (#set! injection.content @pattern)
+    (#set! injection.language "regex")
+  )
+)
