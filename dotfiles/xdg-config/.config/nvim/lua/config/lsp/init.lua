178ba4508facd277394e2b4cafb4d895f7198998
Zane Dufour <zane@znd4.dev>
2024-03-26 08:48:09 -0400

    Revert "add cmp mapping to select previous item"
    This reverts commit c3dbdfba51d056befb300c321a8f5ee031a752c1.

--- dotfiles/xdg-config/.config/nvim/lua/config/lsp/init.lua
+++ dotfiles/xdg-config/.config/nvim/lua/config/lsp/init.lua
@@ -322,7 +322,6 @@ local cmp_config = lsp_zero.defaults.cmp_config({
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
     ["<C-d>"] = cmp.mapping.scroll_docs(4),
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-f>"] = cmp_action.luasnip_jump_forward(),
     ["<C-b>"] = cmp_action.luasnip_jump_backward(),
   }),
