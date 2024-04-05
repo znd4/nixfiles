87a15a31ff702d71bdd8c5dce0ebd3e527173e04
Zane Dufour <zane@znd4.dev>
2024-04-04 21:17:35 -0400

    switch nvidia config to lib.mkIf

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -2,39 +2,39 @@
   inputs,
   hostname,
   config,
+  lib,
   ...
-}: let
-  enabled = builtins.elem hostname ["desktop"];
+}:
+let
+  enabled = builtins.elem hostname [ "desktop" ];
 in
-  if !enabled
-  then {}
-  else {
-    # Copied from nixos.wiki/wiki/Nvidia
-    hardware.opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-    };
-    services.xserver.videoDrivers = ["nvidia"];
+lib.mkIf enabled {
+  # Copied from nixos.wiki/wiki/Nvidia
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  services.xserver.videoDrivers = [ "nvidia" ];
 
-    hardware.nvidia = {
-      modesetting.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
 
-      # Might need to enable this if I have issues sleeping
-      powerManagement.enable = true;
+    # Might need to enable this if I have issues sleeping
+    powerManagement.enable = true;
 
-      # If I ever have a laptop with nvidia dGPU, might want
-      # to revisit this
-      powerManagement.finegrained = false;
+    # If I ever have a laptop with nvidia dGPU, might want
+    # to revisit this
+    powerManagement.finegrained = false;
 
-      # Don't use open-source nvidia driver
-      open = false;
+    # Don't use open-source nvidia driver
+    open = false;
 
-      # Get `nvidia-settings` command
-      nvidiaSettings = true;
+    # Get `nvidia-settings` command
+    nvidiaSettings = true;
 
-      # package = config.boot.kernelPackages.nvidiaPackages.stable;
-      package = config.boot.kernelPackages.nvidiaPackages.production;
-      # package = config.boot.kernelPackages.nvidiaPackages.beta;
-    };
-  }
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+    # package = config.boot.kernelPackages.nvidiaPackages.beta;
+  };
+}
