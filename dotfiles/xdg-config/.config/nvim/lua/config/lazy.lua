66ce8a4b40efd5e2994f2b7720f6c36160b09ae8
Zane Dufour <zane@znd4.dev>
2024-04-06 11:14:22 -0400

    add pyproject.toml and go.mod to project.nvim rooting patterns

--- dotfiles/xdg-config/.config/nvim/lua/config/lazy.lua
+++ dotfiles/xdg-config/.config/nvim/lua/config/lazy.lua
@@ -453,7 +453,7 @@ require("lazy").setup({
         -- refer to the configuration section below
         ignore_lsp = { "null-ls", "terraform_lsp" },
         detection_methods = { "pattern", "lsp" },
-        patterns = { ".git", ".hg", ".svn", "package.json" },
+        patterns = { ".git", ".hg", ".svn", "package.json", "go.mod", "pyproject.toml" },
         show_hidden = true,
         datapath = datapath,
       })
