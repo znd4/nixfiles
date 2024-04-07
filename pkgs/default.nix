b2f6f548e91c03ef2f0259cfacb7946bcbfbcd92
Zane Dufour <zane@znd4.dev>
2024-04-07 10:32:31 -0400

    add hy-auth pkg

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,3 +1,7 @@
-pkgs: {
-
-}
+pkgs:
+let
+  lib = pkgs.lib;
+in
+(lib.attrsets.genAttrs (builtins.filter (f: f != "default.nix") (
+  builtins.attrNames (builtins.readDir ./.)
+)) (name: ((import (./. + "/${name}")) pkgs)))
