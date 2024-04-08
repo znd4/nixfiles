71a79ec72106f3a77446577cacc49f23e3460879
Zane Dufour <zane@znd4.dev>
2024-04-08 12:55:06 -0400

    get lipgloss autoimports working

--- pkgs/hy-auth/main.go
+++ pkgs/hy-auth/main.go
@@ -4,13 +4,22 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"fmt"
+
+	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "hy-auth",
-	Short: "An opinionated wrapper around hydroxide auth",
-}
+var (
+	codeStyle = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.AdaptiveColor{
+		Light: "#040911",
+		Dark:  "#547ebc",
+	})
+	rootCmd = &cobra.Command{
+		Use:   "hy-auth",
+		Short: fmt.Sprintf("An opinionated wrapper around %v", codeStyle.Render("hydroxide auth")),
+	}
+)
 
 func main() {
 	rootCmd.Execute()
