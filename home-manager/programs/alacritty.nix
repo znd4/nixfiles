6fd76cb87248b95804e01a650aa37062a0ac98cb
Zane Dufour <zane@znd4.dev>
2024-04-26 12:10:46 -0400

    add insert mapping in alacritty (should paste)

--- home-manager/programs/alacritty.nix
+++ home-manager/programs/alacritty.nix
@@ -32,6 +32,11 @@
             action = "IncreaseFontSize";
           }
           {
+            key = "Insert";
+            mods = null;
+            action = "Paste";
+          }
+          {
             key = "=";
             mods = "Super";
             action = "IncreaseFontSize";
