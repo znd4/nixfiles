7c7796aaa9807926d026d0c8ac44154d239075cb
Zane Dufour <zane@protectai.com>
2024-05-18 17:12:41 -0700

    add tmux and gitsigns configs

---
+++ nvim/lua/custom/plugins/gitsigns.lua
@@ -0,0 +1,38 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 500,
+    },
+    on_attach = function(bufnr)
+      local wk = require 'which-key'
+      local gitsigns = require 'gitsigns'
+      wk.register({
+        h = {
+          s = { gitsigns.stage_hunk, 'git [s]tage hunk' },
+          r = { gitsigns.reset_hunk, 'git [r]eset hunk' },
+          S = { gitsigns.stage_buffer, 'git [S]tage buffer' },
+          u = { gitsigns.undo_stage_hunk, 'git [u]ndo stage hunk' },
+          R = { gitsigns.reset_buffer, 'git [R]eset buffer' },
+          p = { gitsigns.preview_hunk, 'git [p]review hunk' },
+          b = { gitsigns.blame_line, 'git [b]lame line' },
+          d = { gitsigns.diffthis, 'git [d]iff against index' },
+          D = {
+            function()
+              gitsigns.diffthis '@'
+            end,
+            'git [D]iff against last commit',
+          },
+        },
+        t = {
+          b = { gitsigns.toggle_current_line_blame, '[T]oggle git show [b]lame line' },
+          D = { gitsigns.toggle_deleted, '[T]oggle git show [D]eleted' },
+        },
+      }, { prefix = '<leader>' })
+    end,
+  },
+  dependencies = {
+    'folke/which-key.nvim',
+  },
+}
