c02f535cee1ddc2b1cffadd90e35fec53ebca2d8
Zane Dufour <zane@protectai.com>
2024-05-20 15:44:50 -0700

    add kots completion, by, and kkots

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,8 @@ direnv hook fish | source
 
 source "$(brew --prefix)/share/google-cloud-sdk/path.fish.inc"
 
+kubectl-kots completion fish | source
+
 
 starship init fish | source
 zoxide init fish | source
@@ -15,5 +17,7 @@ zoxide init fish | source
 alias g=git
 alias vi=nvim
 alias bh="bat -l help"
+alias by="bat -l yaml"
+alias kkots="kubectl-kots"
 
 abbr k kubectl
