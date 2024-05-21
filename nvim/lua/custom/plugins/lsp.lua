284482a0428a95721c8e2bf5872397ba669a6abf
Zane Dufour <zane@protectai.com>
2024-05-21 09:03:18 -0700

    add yamlls

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -67,6 +67,15 @@ return {
       -- (Optional) Configure lua language server for neovim
       local lua_opts = lsp_zero.nvim_lua_ls()
       require('lspconfig').lua_ls.setup(lua_opts)
+      require('lspconfig').yamlls.setup {
+        settings = {
+          yaml = {
+            schemas = {
+              ['https://json.schemastore.org/github-action.json'] = '/.github/workflows/*',
+            },
+          },
+        },
+      }
       require('lspconfig').basedpyright.setup {}
     end,
   },
