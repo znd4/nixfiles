72a45d5fb64a3b20c6e1351d97dbf7a4bd62feea
Zane Dufour <zane@protectai.com>
2024-05-29 13:28:18 -0400

    add tiltfile configuration

---
+++ nvim/ftplugin/tiltfile.lua
@@ -0,0 +1,8 @@
+-- Set the syntax highlighting to starlark
+vim.bo.commentstring = '#%s'
+
+-- Configure indentation settings
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.expandtab = true
--- nvim/init.lua
+++ nvim/init.lua
@@ -845,6 +845,7 @@ require('lazy').setup({
       require('nvim-treesitter.install').prefer_git = true
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
+      vim.treesitter.language.register('skylark', { 'tiltfile' })
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
