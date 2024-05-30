9abb01b8f24c73325b021de126aaaa0cedaebed2
Zane Dufour <zane@protectai.com>
2024-05-30 13:02:12 -0400

    change toggleterm mapping to ctrl-j

--- nvim/lua/custom/plugins/toggleterm.lua
+++ nvim/lua/custom/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
   version = '*',
   shell = 'fish',
   opts = {
-    open_mapping = [[<c-t>]],
+    open_mapping = [[<c-j>]],
     direction = 'tab', -- "horizontal", "vertical", "float"
     shell = 'fish',
   },
