225d7cbe27d0e139c6e0100f3305a5f2e75307b0
Zane Dufour <zane@protectai.com>
2024-05-30 12:53:20 -0400

    add toggleterm to nvim

---
+++ nvim/lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  shell = 'fish',
+  opts = {
+    open_mapping = [[<c-t>]],
+    direction = 'tab', -- "horizontal", "vertical", "float"
+    shell = 'fish',
+  },
+}
