0b7eea37adc3e3990b4fdd1063d5a6d441c938cc
Zane Dufour <zane@protectai.com>
2024-07-23 14:49:03 -0400

    switch everything to catppuccin

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     options = {
-      theme = "tokyonight",
+      theme = "catppuccin",
     },
     sections = {
       lualine_c = { { 'filename', path = 1 } }
