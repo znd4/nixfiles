1fd5cd105a4e7439612c220872340d822f3257a6
Zane Dufour <zane@protectai.com>
2024-11-11 13:57:18 -0500

    actionlint

--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -5,6 +5,15 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
+      lint.linters.actionlint = {
+        cmd = 'actionlint',
+        stdin = true, -- or false if it doesn't support content input via stdin. In that case the filename is automatically added to the arguments.
+        append_fname = true, -- Automatically append the file name to `args` if `stdin = false` (default: true)
+        args = {}, -- list of arguments. Can contain functions with zero arguments that will be evaluated once the linter is used.
+        stream = nil, -- ('stdout' | 'stderr' | 'both') configure the stream to which the linter outputs the linting result.
+        ignore_exitcode = false, -- set this to true if the linter exits with a code != 0 and that's considered normal.
+        env = nil, -- custom environment table to use with the external process. Note that this replaces the *entire* environment, it is not additive.
+      }
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
       }
@@ -44,10 +53,20 @@ return {
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      local events = { 'BufEnter', 'BufWritePost', 'InsertLeave' }
+
+      vim.api.nvim_create_autocmd(events, {
         group = lint_augroup,
         callback = function()
-          require('lint').try_lint()
+          local bufname = vim.api.nvim_buf_get_name(0)
+          local is_yaml = vim.bo.filetype == 'yaml'
+          local is_github_action = is_yaml and (bufname:match 'action%.ya?ml$' or bufname:match '%.github/workflows/.*%.ya?ml$')
+
+          if is_github_action then
+            require('lint').try_lint 'actionlint'
+          else
+            require('lint').try_lint()
+          end
         end,
       })
     end,
