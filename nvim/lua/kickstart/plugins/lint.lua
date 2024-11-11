42dabada750a82096ac2af78b1abeb2a9683e8c5
Zane Dufour <zane@protectai.com>
2024-11-11 13:57:23 -0500

    golangcilint

--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -16,6 +16,7 @@ return {
       }
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        go = { 'golangcilint' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
