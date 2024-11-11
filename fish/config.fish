9c4c2e7c228bdfa54ac7e876201961a17f955e4e
Zane Dufour <zane@protectai.com>
2024-11-11 14:02:28 -0500

    carapace configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
+    atuin init fish | source
 end
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.cargo/bin
@@ -18,15 +19,27 @@ zoxide init fish | source
 
 # source ~/.config/op/plugins.sh
 
-alias g=git
 alias vi=nvim
 alias bh="bat -l help"
-alias by="bat -l yaml"
-alias kkots="kubectl-kots"
 alias ipy="py -m IPython"
 
 abbr k kubectl
+abbr g git
+# abbr gt git town
 abbr ky kubectl get -o yaml
+abbr kk k9s
+abbr kr kubectl --context rancher-desktop
+abbr pc pre-commit
+abbr -a by --position anywhere --set-cursor "% | bat -l yaml"
+abbr -a bh --position anywhere --set-cursor "% | bat -l help"
+
+##### carapace.sh completions
+set -gx CARAPACE_EXCLUDES kubectl
+set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
+mkdir -p ~/.config/fish/completions
+carapace --list | awk '{print $1}' | xargs -P0 -I{} touch ~/.config/fish/completions/{}.fish # disable auto-loaded completions (#185)
+carapace _carapace | source
+
 
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
 set --export --prepend PATH "/Users/znd4/.rd/bin"
