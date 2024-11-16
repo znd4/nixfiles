127b549c2d58a988816cca43506ebf4760d702a6
Zane Dufour <zane@znd4.dev>
2024-11-16 08:53:45 -0700

    make programs.jujutsu.package use nixos unstable

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -82,6 +82,7 @@ in
   };
   # TODO: Enable saved WIFI connection credentials
   programs.jujutsu.enable = true; # TODO - try this out
+  programs.jujutsu.package = inputs.nixos-unstable.legacyPackages.${system}.jujutsu;
   programs.thefuck = {
     enable = true;
   };
