50db28566bf7d270c06df1751bd05ae862cca0e4
Zane Dufour <zane@znd4.dev>
2024-12-27 17:11:40 -0600

    hide ghostty titlebar

--- home-manager/programs/ghostty.nix
+++ home-manager/programs/ghostty.nix
@@ -21,6 +21,7 @@
       theme = "tokyonight-storm";
       shell-integration-features = "no-cursor";
       font-family = "MonaspiceAr Nerd Font Mono";
+      macos-titlebar-style = "hidden";
       font-style = "Medium";
       font-family-italic = "MonaspiceRn Nerd Font Mono";
       font-style-italic = "Italic";
