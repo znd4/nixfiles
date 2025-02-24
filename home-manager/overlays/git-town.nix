35abdb7aab71d67b7505b0c426b98d6500f60b8d
Zane Dufour <zane@znd4.dev>
2025-02-23 23:50:27 -0500

    refactor: Switch git-town back to overrideAttrs

--- home-manager/overlays/git-town.nix
+++ home-manager/overlays/git-town.nix
@@ -3,21 +3,13 @@
   ...
 }:
 (final: prev: {
-  git-town = pkgs.writeShellApplication {
-    name = "git-town";
-    runtimeInputs = with pkgs; [ bash ];
-    derivationArgs = {
-      nativeBuildInputs = [ pkgs.installShellFiles ];
-      postInstall = ''
-        installShellCompletion \
-          --cmd git-town \
-          --bash <(${prev.git-town}/bin/git-town completions bash) \
-          --fish <(${prev.git-town}/bin/git-town completions fish) \
-          --zsh <(${prev.git-town}/bin/git-town completions zsh)
-      '';
-    };
-    text = ''
-      GITHUB_AUTH_TOKEN=$(op read "op://Personal/GitHub Personal Access Token/token") ${prev.git-town}/bin/git-town "$@"
+  git-town = prev.git-town.overrideAttrs (oldAttrs: {
+    buildInputs = (oldAttrs.buildInputs or [ ]) ++ [ prev.makeWrapper ];
+    postInstall = ''
+      ${oldAttrs.postInstall or ""}
+      rm $out/bin/git-town
+      makeWrapper ${prev.git-town}/bin/git-town $out/bin/git-town \
+        --run 'export GITHUB_AUTH_TOKEN=$(op read "op://Personal/GitHub Personal Access Token/token")'
     '';
-  };
+  });
 })
