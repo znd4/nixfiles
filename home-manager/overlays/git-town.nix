b7ef04f94c67967a9387468eafdfe446b4f8ee28
Zane Dufour <zane@znd4.dev>
2025-02-23 22:25:14 -0500

    git-town: re-add shell completions

--- home-manager/overlays/git-town.nix
+++ home-manager/overlays/git-town.nix
@@ -3,7 +3,21 @@
   ...
 }:
 (final: prev: {
-  git-town = pkgs.writeShellScriptBin "git-town" ''
-    GITHUB_AUTH_TOKEN=$(op read "op://Personal/GitHub Personal Access Token/token") ${prev.git-town}/bin/git-town "$@"
-  '';
+  git-town = pkgs.writeShellApplication {
+    name = "git-town";
+    runtimeInputs = with pkgs; [ bash ];
+    derivationArgs = {
+      nativeBuildInputs = [ pkgs.installShellFiles ];
+      postInstall = ''
+        installShellCompletion \
+          --cmd git-town \
+          --bash <(${prev.git-town}/bin/git-town completions bash) \
+          --fish <(${prev.git-town}/bin/git-town completions fish) \
+          --zsh <(${prev.git-town}/bin/git-town completions zsh)
+      '';
+    };
+    text = ''
+      GITHUB_AUTH_TOKEN=$(op read "op://Personal/GitHub Personal Access Token/token") ${prev.git-town}/bin/git-town "$@"
+    '';
+  };
 })
