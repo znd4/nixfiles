867a16ec0899bc5235edf60337953cacca21dea3
Zane Dufour <zane@znd4.dev>
2025-03-08 14:39:37 -0500

    fix: don't use hyprland flake module

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -10,7 +10,6 @@
     # trusted-substituters = [ "https://hyprland.cachix.org" ];
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
-  imports = [ inputs.hyprland.nixosModules.default ];
 
   services.displayManager.sddm = {
     enable = true;
