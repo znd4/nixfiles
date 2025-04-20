9d10394b252c439185285fae5e3231d798ffb432
Zane Dufour <zane@znd4.dev>
2025-04-19 22:32:36 -0400

    disable noice lsp stuff

--- xdg-config/nvim/lua/custom/plugins/noice.lua
+++ xdg-config/nvim/lua/custom/plugins/noice.lua
@@ -2,13 +2,17 @@ return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   opts = {
+    lsp = {
+      hover = { enabled = false },
+      signature = { enabled = false },
+    },
     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
     override = {
       ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
       ['vim.lsp.util.stylize_markdown'] = true,
     },
     presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
+      -- bottom_search = true, -- use a classic bottom cmdline for search
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
