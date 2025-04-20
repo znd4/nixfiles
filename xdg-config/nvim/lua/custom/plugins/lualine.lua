ddfc71f42593c7ae7645a1984ec4ca75ad3b68d0
Zane Dufour <zane@znd4.dev>
2025-04-19 22:32:26 -0400

    add catpuccin as lualine dep

--- xdg-config/nvim/lua/custom/plugins/lualine.lua
+++ xdg-config/nvim/lua/custom/plugins/lualine.lua
@@ -1,12 +1,12 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'catppuccin/nvim' },
   opts = {
     options = {
-      theme = "catppuccin",
+      theme = 'catppuccin',
     },
     sections = {
-      lualine_c = { { 'filename', path = 1 } }
+      lualine_c = { { 'filename', path = 1 } },
     },
-  }
+  },
 }
