95e33bfb83974e896fa5e49e7eb48a91709a9a03
Zane Dufour <zane@znd4.dev>
2025-05-01 18:05:34 -0400

    more sunshine configuration

--- nixos/modules/sunshine.nix
+++ nixos/modules/sunshine.nix
@@ -4,5 +4,26 @@
 {
   services.sunshine = {
     enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
+  };
+  networking.firewall = {
+    allowedTCPPorts = [
+      47984
+      47989
+      47990
+      48010
+    ];
+    allowedUDPPortRanges = [
+      {
+        from = 47998;
+        to = 48000;
+      }
+      {
+        from = 8000;
+        to = 8010;
+      }
+    ];
   };
 }
