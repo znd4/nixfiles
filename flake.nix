766e646e39c2dd66f0f7dff6880ea7bea3ef6da8
Zane Dufour <zane@znd4.dev>
2025-07-04 14:41:40 -0400

    new git branches

--- flake.nix
+++ flake.nix
@@ -28,7 +28,8 @@
     nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-trunk.url = "github:NixOS/nixpkgs/master";
     nixpkgs-24_11.url = "github:NixOS/nixpkgs/nixos-24.11-small";
-    nixpkgs-git-town-21_1_0.url = "github:nixos/nixpkgs/pull/419405/head";
+    # nixpkgs-git-town-21_1_0.url = "github:nixos/nixpkgs/pull/419405/head";
+    nixpkgs-git-town-21_2_0.url = "github:znd4/nixpkgs/git-town-21.2.0";
 
     nil.url = "github:oxalica/nil";
     nil.inputs.nixpkgs.follows = "nixpkgs";
--- home-manager/overlays/git-town.nix
+++ home-manager/overlays/git-town.nix
@@ -6,17 +6,5 @@
 }:
 (final: prev: {
   # git-town = prev.git-town.overrideAttrs (oldAttrs: {
-  git-town =
-    let
-      git-town = inputs.nixpkgs-git-town-21_1_0.legacyPackages.${system}.git-town;
-    in
-    git-town.overrideAttrs (oldAttrs: {
-      buildInputs = (oldAttrs.buildInputs or [ ]) ++ [ prev.makeWrapper ];
-      postInstall = ''
-        ${oldAttrs.postInstall or ""}
-        rm $out/bin/git-town
-        makeWrapper ${git-town}/bin/git-town $out/bin/git-town \
-          --run 'export GITHUB_AUTH_TOKEN=$(op read "op://Personal/GitHub Personal Access Token/token")'
-      '';
-    });
+  git-town = inputs.nixpkgs-git-town-21_2_0.legacyPackages.${system}.git-town;
 })
