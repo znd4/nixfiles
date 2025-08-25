2e6dc65217c751460d2e1ca35eee2250e046fc2d
Zane Dufour <zane@znd4.dev>
2025-08-25 19:32:45 -0400

    change gh configuration

---
+++ home-manager/programs/gh.nix
@@ -0,0 +1,38 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  cfg = config.programs.okta-aws-cli-assume-role;
+in
+{
+  options.programs.op-gh = {
+    enable = lib.mkOption {
+      default = true;
+      example = true;
+      description = "Whether to install a `gh` script that wraps the actual `gh` CLI with 1password plugin authentication";
+      type = lib.types.bool;
+    };
+  };
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      (final.writeShellApplication {
+        name = "gh";
+        runtimeInputs = [
+          final._1password-cli
+          prev.gh
+        ];
+        text = ''
+          #!${final.runtimeShell}
+          # 'exec' replaces the shell process with the 'op' process, which is
+          # more efficient and handles signals correctly.
+          # "$@" forwards all arguments, preserving spaces and special characters.
+          exec op plugin run -- gh "$@"
+        '';
+      })
+    ];
+  };
+}
