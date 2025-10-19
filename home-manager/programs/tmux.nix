8c2d88ad3394ff350d8fe8668fe94df8300b8925
Zane Dufour <zane@znd4.dev>
2025-10-19 15:38:14 -0400

    switch to more recommended settings

--- home-manager/programs/tmux.nix
+++ home-manager/programs/tmux.nix
@@ -108,6 +108,15 @@
         extraConfig = ''
           set -g @catppuccin_flavor 'macchiato'
           set -g @catppuccin_window_status_style 'rounded'
+
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -agF status-right "#{E:@catppuccin_status_cpu}"
+          set -ag status-right "#{E:@catppuccin_status_session}"
+          set -ag status-right "#{E:@catppuccin_status_uptime}"
+          set -agF status-right "#{E:@catppuccin_status_battery}"
         '';
       }
       pain-control
