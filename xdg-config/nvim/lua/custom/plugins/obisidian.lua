366a292356287fbde8c574df4944a64fe0e40efa
Zane Dufour <zane@znd4.dev>
2025-10-19 22:03:54 -0400

    install nvim

---
+++ xdg-config/nvim/lua/custom/plugins/obisidian.lua
@@ -0,0 +1,40 @@
+return {
+  'epwalsh/obsidian.nvim',
+  version = 'v3.9.0', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+  --   "BufReadPre path/to/my-vault/**.md",
+  --   "BufNewFile path/to/my-vault/**.md",
+  -- },
+  dependencies = {
+    -- Required.
+    'nvim-lua/plenary.nvim',
+
+    -- see below for full list of optional dependencies 👇
+  },
+  keys = {
+    {
+      '<leader>od',
+      '<cmd>ObsidianToday<cr>',
+      desc = 'Obsidian Daily note',
+    },
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'personal',
+        path = '~/Documents/obsidian-gtd-main',
+      },
+      -- {
+      --   name = 'work',
+      --   path = '~/vaults/work',
+      -- },
+    },
+
+    -- see below for full list of options 👇
+  },
+}
