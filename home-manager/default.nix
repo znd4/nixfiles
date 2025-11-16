191a6e6ba7f9100f1de28cdfb989370f68c5a7f1
Zane Dufour <zane@znd4.dev>
2025-11-15 22:31:54 -0500

    fix

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -23,7 +23,7 @@ let
   };
   dotConfig = "${inputs.self}/dotfiles/xdg-config/.config";
   system = pkgs.stdenv.system;
-  personal_python = inputs.nixpkgs.pkgs-24_11.legacyPackages.${system}.python3.withPackages (
+  personal_python = inputs.nixpkgs.legacyPackages.${system}.python3.withPackages (
     ps:
     # personal_python = inputs.nixpkgs-main.legacyPackages.${system}.python3.withPackages (ps:
     with ps; [
