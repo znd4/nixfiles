193a50f802a59bac9f1c1ca55d836fb0c474052e
Zane Dufour <zane@znd4.dev>
2025-11-18 10:35:07 -0500

    change blink-cmp from opt to config
    **why**
    we need minuet to be available

--- xdg-config/nvim/lua/custom/plugins/lsp.lua
+++ xdg-config/nvim/lua/custom/plugins/lsp.lua
@@ -15,67 +15,67 @@ return {
       -- add friendly snippets to luasnip
       require('luasnip.loaders.from_vscode').lazy_load()
     end,
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept, C-n/C-p for up/down)
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys for up/down)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      --
-      -- All presets have the following mappings:
-      -- C-space: Open menu or open docs if already open
-      -- C-e: Hide menu
-      -- C-k: Toggle signature help
-      --
-      -- See the full "keymap" documentation for information on defining your own keymap.
-      keymap = {
-        -- Manually invoke minuet completion.
-        ['<A-y>'] = require('minuet').make_blink_map(),
-        preset = 'default',
-      },
-      sources = {
-        -- Enable minuet for autocomplete
-        default = { 'lsp', 'path', 'buffer', 'snippets', 'minuet' },
-        -- For manual completion only, remove 'minuet' from default
-        providers = {
-          minuet = {
-            name = 'minuet',
-            module = 'minuet.blink',
-            async = true,
-            -- Should match minuet.config.request_timeout * 1000,
-            -- since minuet.config.request_timeout is in seconds
-            timeout_ms = 3000,
-            score_offset = 50, -- Gives minuet higher priority among suggestions
+    config = function()
+      require('blink').setup {
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept, C-n/C-p for up/down)
+        -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys for up/down)
+        -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+        --
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help
+        --
+        -- See the full "keymap" documentation for information on defining your own keymap.
+        keymap = {
+          -- Manually invoke minuet completion.
+          ['<A-y>'] = require('minuet').make_blink_map(),
+          preset = 'default',
+        },
+        sources = {
+          -- Enable minuet for autocomplete
+          default = { 'lsp', 'path', 'buffer', 'snippets', 'minuet' },
+          -- For manual completion only, remove 'minuet' from default
+          providers = {
+            minuet = {
+              name = 'minuet',
+              module = 'minuet.blink',
+              async = true,
+              -- Should match minuet.config.request_timeout * 1000,
+              -- since minuet.config.request_timeout is in seconds
+              timeout_ms = 3000,
+              score_offset = 50, -- Gives minuet higher priority among suggestions
+            },
           },
         },
-      },
 
-      completion = {
-        -- Manually invoke minuet completion.
-        ['<A-y>'] = require('minuet').make_blink_map(),
-        documentation = { auto_show = true },
-      },
+        completion = {
+          -- Manually invoke minuet completion.
+          ['<A-y>'] = require('minuet').make_blink_map(),
+          documentation = { auto_show = true },
+        },
 
-      appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- Will be removed in a future release
-        use_nvim_cmp_as_default = true,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono',
-      },
+        appearance = {
+          -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+          -- Useful for when your theme doesn't support blink.cmp
+          -- Will be removed in a future release
+          use_nvim_cmp_as_default = true,
+          -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+          -- Adjusts spacing to ensure icons are aligned
+          nerd_font_variant = 'mono',
+        },
 
-      -- preset snippets
-      snippets = { preset = 'luasnip' },
+        -- preset snippets
+        snippets = { preset = 'luasnip' },
 
-      -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
-      fuzzy = { implementation = 'prefer_rust_with_warning' },
-    },
+        -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
+        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+        --
+        -- See the fuzzy documentation for more information
+        fuzzy = { implementation = 'prefer_rust_with_warning' },
+      }
+    end,
     opts_extend = { 'sources.default' },
   },
 
